f2f4accc19d1b9be6f57d0fb00418795e0eb9ba2
Daniel Newman <dwnewman78@gmail.com>
2019-01-31 15:58:22 +0000

    Tweaked email guesser

--- git-scripts/email-guess.sh
+++ git-scripts/email-guess.sh
@@ -2,7 +2,7 @@
 remote="$(git remote -v | awk '/\(push\)$/ {print $2}')"
 email=dwnewman78@gmail.com # default
 echo "$remote"
-[[ $remote == *newyu* ]] && email=dnewman@newyu.com
+[[ $remote == *newyuinc* ]] && email=dnewman@newyu.com
 [[ $remote == *digitalreasoning* ]] && email=daniel.newman@digitalreasoning.com
 [[ $remote == *github.com:floatingman* ]] && email=dwnewman78@gmail.com
 
