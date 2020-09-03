949a391de257dbf181886f86c15260c27cb79f4a
Daniel Newman <dwnewman78@gmail.com>
2020-09-02 22:58:20 -0500

    Added nix to bash

--- bashrc.d/603-exports.bash
+++ bashrc.d/603-exports.bash
@@ -103,3 +103,5 @@ fi
 if [ -d $HOME/.jx/bin ]; then
     _add_to_path "$HOME/.jx/bin"
 fi
+
+if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
