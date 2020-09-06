ce3f2bba9f21cd4a933d03023d56b4b8d02c0fa3
Daniel Newman <dwnewman78@gmail.com>
2020-09-05 22:20:46 -0500

    Removed manual nix install

--- bashrc.d/603-exports.bash
+++ bashrc.d/603-exports.bash
@@ -103,5 +103,3 @@ fi
 if [ -d $HOME/.jx/bin ]; then
     _add_to_path "$HOME/.jx/bin"
 fi
-
-if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
