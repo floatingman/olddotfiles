9d20ac959181ef647dd1b2ea93a83433ba9ad172
Daniel Newman <dwnewman78@gmail.com>
2020-11-30 14:39:04 -0600

    Trying out a different Window Manager

--- bashrc.d/990-closing.bash
+++ bashrc.d/990-closing.bash
@@ -1,3 +1,16 @@
 
 [[ -r ~/.bash_personal   ]] && source ~/.bash_personal
 [[ -r ~/.bash_private   ]] && source ~/.bash_private
+
+if pacman -Qs libxft-bgra >/dev/null 2>&1; then
+	# Start graphical server on user's current tty if not already running.
+	[ "$(tty)" = "/dev/tty1" ] && ! pidof -s Xorg >/dev/null 2>&1 && exec startx
+else
+	echo "\033[31mIMPORTANT\033[0m: Note that \033[32m\`libxft-bgra\`\033[0m must be installed for this build of dwm.
+Please run:
+	\033[32myay -S libxft-bgra-git\033[0m
+and replace \`libxft\`. Afterwards, you may start the graphical server by running \`startx\`."
+fi
+
+# Switch escape and caps if tty and no passwd required:
+sudo -n loadkeys ${XDG_DATA_HOME:-$HOME/.local/share}/larbs/ttymaps.kmap 2>/dev/null
