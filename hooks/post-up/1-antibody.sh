11612023cf5005d7e6bdc98d64d706f2b8d59e0c
Daniel Newman <dwnewman78@gmail.com>
2020-12-31 11:53:04 -0600

    Changed doom config to literate

--- hooks/post-up/1-antibody.sh
+++ hooks/post-up/1-antibody.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if hash antibody 2> /dev/null; then
-    antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+    antibody bundle < ~/.dotfiles/zsh_plugins.txt > ~/.zsh_plugins.sh
 fi
