1e74a87d8d726ebe865dea85c27a83e7760322c0
Daniel Newman <dwnewman78@gmail.com>
2022-03-01 14:44:12 -0600

    Updated zsh again

--- hooks/post-up/1-links.sh
+++ hooks/post-up/1-links.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ln -sf $HOME/.dotfiles/config/zsh/.zshrc $HOME/.zshrc
-ln -sf $HOME/.dotfiles/config/zsh/.zprofile $HOME/.zprofile
+ln -sf $HOME/.dotfiles/config/zsh/.zshrc $HOME/.config/zsh/.zshrc
+ln -sf $HOME/.dotfiles/config/zsh/.zprofile $HOME/.config/zsh/.zprofile
